47fd8695a536a2385ff5c186f8cb38e8eb2c1a10
Moe Jangda <moe@tbd.email>
2024-02-06 13:50:47 -0600

    address PR comment
    Co-Authored-By: Wes <wesbillman@users.noreply.github.com>

--- packages/web5/test/dids/did_web_test.dart
+++ packages/web5/test/dids/did_web_test.dart
@@ -3,7 +3,7 @@ import 'package:web5/src/dids/did_web/did_web.dart';
 
 void main() {
   group('DidWeb', () {
-    test('should work', () async {
+    test('should resolve successfully', () async {
       final result = await DidWeb.resolve('did:web:www.linkedin.com');
       expect(result.didDocument, isNotNull);
 
