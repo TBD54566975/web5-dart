27b65588227b5fe37302f93ad77cb864ceeae9c5
Wes <wesbillman@users.noreply.github.com>
2024-02-09 11:30:36 -0700

    feat: add did_web export

--- packages/web5/lib/src/dids.dart
+++ packages/web5/lib/src/dids.dart
@@ -4,4 +4,5 @@ export './dids/data_models.dart';
 export './dids/did_resolver.dart';
 export './dids/did_dht/did_dht.dart';
 export './dids/did_jwk/did_jwk.dart';
+export './dids/did_web/did_web.dart';
 export './dids/did_method_resolver.dart';
