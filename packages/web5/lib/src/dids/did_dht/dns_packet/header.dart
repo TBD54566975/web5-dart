eb6f54dd1219a05d33720f8c706ee4416f733a0e
Stone Pack <stone.pack@icloud.com>
2024-03-22 14:15:37 -0700

    added documentation and updated required fields for DNS packet header

--- packages/web5/lib/src/dids/did_dht/dns_packet/header.dart
+++ packages/web5/lib/src/dids/did_dht/dns_packet/header.dart
@@ -4,20 +4,49 @@ import 'package:web5/src/dids/did_dht/dns_packet/opcode.dart';
 import 'package:web5/src/dids/did_dht/dns_packet/rcode.dart';
 
 class DnsHeader {
+  /// Identifier assigned by the program that generates the query.
   int id;
+
+  /// Whether this message is a query (0), or a response (1)
   bool qr;
+
+  /// Specifies kind of query in this message.
   DnsOpCode opcode;
-  bool aa;
+
+  /// Specifies that the responding name server is an authority for the domain name in question section.
+  bool? aa;
+
+  /// Specifies whether the message was truncated.
   bool tc;
+
+  /// Directs the name server to pursue the query recursively
   bool rd;
-  bool ra;
+
+  /// Set or cleared in a response, and denotes whether recursive query support is available in the name server
+  bool? ra;
+
+  /// Reserved for future use, always set to 0.
   bool z;
-  bool ad;
-  bool cd;
+
+  /// TODO: Find documentation for this field
+  bool? ad;
+
+  /// TODO: Find documentation for this field
+  bool? cd;
+
+  /// Response code
   DnsRCode rcode;
+
+  /// Number of entries in the question section.
   int qdcount;
+
+  /// Number of resource records in the answer section.
   int ancount;
+
+  /// Number of name server resource records in the authority records section.
   int nscount;
+
+  /// Number of resource records in the additional records section.
   int arcount;
 
   get numQuestions => qdcount;
@@ -31,11 +60,11 @@ class DnsHeader {
     required this.id,
     required this.qr,
     required this.opcode,
-    required this.aa,
+    this.aa,
     required this.tc,
     required this.rd,
-    required this.ra,
-    required this.z,
+    this.ra,
+    this.z = false,
     required this.ad,
     required this.cd,
     required this.rcode,
