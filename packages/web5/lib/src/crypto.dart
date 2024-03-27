f87858b3b36b6f693b080d428769a44fb9169a79
Stone Pack <stone.pack@icloud.com>
2024-03-27 12:46:42 -0700

    reverted some structure changes to avoid circular dependencies

--- packages/web5/lib/src/crypto.dart
+++ packages/web5/lib/src/crypto.dart
@@ -6,16 +6,6 @@ export 'crypto/encryption/secp256k1.dart';
 
 export 'crypto/jwk/jwk.dart';
 
-export 'crypto/jws/decoded.dart';
-export 'crypto/jws/jws.dart';
-export 'crypto/jws/header.dart';
-
-export 'crypto/jwt/decoded.dart';
-export 'crypto/jwt/claims.dart';
-export 'crypto/jwt/encoded.dart';
-export 'crypto/jwt/header.dart';
-export 'crypto/jwt/jwt.dart';
-
 export 'crypto/key_managers/key_manager.dart';
 export 'crypto/key_managers/in_memory_key_manager.dart';
 export 'crypto/key_managers/ios_key_manager.dart';
