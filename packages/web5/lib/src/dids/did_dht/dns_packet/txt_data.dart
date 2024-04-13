330ad1a8d596be7c7c5cfe60359960486c5e253e
Ethan Lee <ethanl@tbd.email>
2024-04-12 22:35:57 -0700

    add `encode` to `DnsTxtData`

--- packages/web5/lib/src/dids/did_dht/dns_packet/txt_data.dart
+++ packages/web5/lib/src/dids/did_dht/dns_packet/txt_data.dart
@@ -9,7 +9,7 @@ class DnsTxtData implements RData {
   @override
   int numBytes;
 
-  DnsTxtData._(this.value, this.numBytes);
+  DnsTxtData(this.value, this.numBytes);
 
   factory DnsTxtData.decode(Uint8List buf, int offset) {
     final originalOffset = offset;
@@ -31,7 +31,30 @@ class DnsTxtData implements RData {
     }
 
     final numBytes = offset - originalOffset;
-    return DnsTxtData._(data, numBytes);
+    return DnsTxtData(data, numBytes);
+  }
+
+  @override
+  Uint8List encode({Uint8List? buf, int offset = 0}) {
+    buf ??= Uint8List(encodingLength());
+    final byteData = ByteData.sublistView(buf);
+
+    final originalOffset = offset;
+    offset += 2; // Reserve space for the total length
+
+    for (String str in value) {
+      final encodedStr = utf8.encode(str);
+      if (encodedStr.length > 255) {
+        throw FormatException('Individual TXT record exceeds 255 bytes');
+      }
+      buf[offset++] = encodedStr.length; // Write the length of the string
+      buf.setRange(offset, offset + encodedStr.length, encodedStr);
+      offset += encodedStr.length;
+    }
+
+    byteData.setUint16(originalOffset, offset - originalOffset - 2, Endian.big);
+
+    return buf;
   }
 
   int encodingLength() {
@@ -40,10 +63,5 @@ class DnsTxtData implements RData {
       length += utf8.encode(buf).length + 1;
     }
     return length;
-  }
-
-  @override
-  DnsTxtData encode() {
-    throw UnimplementedError();
   }
 }
