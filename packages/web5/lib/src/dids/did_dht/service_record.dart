7afa4e44fb4d9712f5f580aa10358d1692499c78
Ethan Lee <ethanl@tbd.email>
2024-04-18 22:20:19 -0700

    fill out `ServiceRecord`
    Co-Authored-By: Jiyoon Koo <118510103+jiyoontbd@users.noreply.github.com>

--- packages/web5/lib/src/dids/did_dht/service_record.dart
+++ packages/web5/lib/src/dids/did_dht/service_record.dart
@@ -1,18 +1,80 @@
 import 'package:web5/src/dids/did_dht/dns/answer.dart';
+import 'package:web5/src/dids/did_dht/dns/name.dart';
+import 'package:web5/src/dids/did_dht/dns/record_class.dart';
+import 'package:web5/src/dids/did_dht/dns/record_type.dart';
 import 'package:web5/src/dids/did_dht/dns/txt_data.dart';
 import 'package:web5/web5.dart';
 
-// TODO: fill these out next
 class ServiceRecord {
-  factory ServiceRecord.fromService(DidService service) {
-    throw UnimplementedError();
+  static Set<String> txtEntryNames = {'id', 't', 'se'};
+
+  static Answer<TxtData> toTxtRecord(DidService service) {
+    return Answer<TxtData>(
+      name: RecordName('https://diddht.tbddev.org'),
+      type: RecordType.TXT,
+      klass: RecordClass.IN,
+      data: TxtData([
+        'id=${service.id};t=${service.type};se=${service.serviceEndpoint}',
+      ]),
+      ttl: 0,
+    );
   }
 
-  factory ServiceRecord.fromRecord(Answer<TxtData> record) {
-    throw UnimplementedError();
+  static DidService toDidService(Answer<TxtData> record) {
+    final txtData = record.data;
+
+    final Map<String, List<String>> relationshipsMap = {};
+
+    // TODO: is this the right way to index into txtData.value?
+    for (final entry in txtData.value[0].split(';')) {
+      final splitEntry = entry.split('=');
+
+      if (splitEntry.length != 2) {
+        // TODO: figure out more appopriate resolution error to use.
+        print('oops');
+      }
+
+      final [property, values] = splitEntry;
+      final splitValues = values.split(',');
+
+      if (!txtEntryNames.contains(property)) {
+        continue;
+      }
+
+      for (final value in splitValues) {
+        relationshipsMap[property] ??= [];
+        relationshipsMap[property]!.add(value);
+      }
+    }
+
+    final id = relationshipsMap['id']?.first;
+    final type = relationshipsMap['t']?.first;
+    final serviceEndpoint = relationshipsMap['se']?.first;
+
+    return DidService(
+      id: id ?? '',
+      type: type ?? '',
+      serviceEndpoint: serviceEndpoint ?? '',
+    );
   }
 
-  DidService toService() {
-    throw UnimplementedError();
-  }
+  // factory ServiceRecord.fromService(DidService service) {
+  //   return ServiceRecord(
+  //     id: service.id,
+  //     type: service.type,
+  //     rData: TxtData([
+  //       'id=${service.id};t=${service.type};se=${service.serviceEndpoint};',
+  //     ]),
+  //     ttl: 7200,
+  //     serviceEndpoint: service.serviceEndpoint,
+  //   );
+  // }
+
+  // DidService toService() {
+  //   return DidService(
+  //     id: id,
+  //     type: type,
+  //     serviceEndpoint: serviceEndpoint ?? '',
+  //   );
+  // }
 }
