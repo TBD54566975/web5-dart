a40b251eb272bc9dbe7de4fa2ab5297e16d1d72f
Moe Jangda <moe@tbd.email>
2024-04-23 12:02:04 -0500

    implement `VerificationMethodRecord.toVerificationMethod`

--- packages/web5/lib/src/dids/did_dht/vm_record.dart
+++ packages/web5/lib/src/dids/did_dht/vm_record.dart
@@ -5,7 +5,20 @@ import 'package:web5/src/dids/did_dht/dns_packet/record_type.dart';
 import 'package:web5/src/dids/did_dht/dns_packet/txt_data.dart';
 import 'package:web5/web5.dart';
 
+/// Relevant [spec text](https://did-dht.com/#verification-method-record)
 class VerificationMethodRecord {
+  /// relevant [spec text](https://did-dht.com/registry/index.html#key-type-index)
+  static final Map<String, String> _keyTypeToIndex = {
+    Ed25519.crv: '0',
+    Secp256k1.crv: '1',
+  };
+
+  /// relevant [spec text](https://did-dht.com/registry/index.html#key-type-index)
+  static final Map<String, AlgorithmId> _indexToKeyType = {
+    '0': AlgorithmId.ed25519,
+    '1': AlgorithmId.secp256k1,
+  };
+
   static Set<String> txtEntryNames = {'id', 't', 'k', 'a', 'c'};
 
   static Answer<TxtData> toTxtRecord(
@@ -13,20 +26,51 @@ class VerificationMethodRecord {
     DidVerificationMethod method,
   ) {
     final pubKey = Crypto.publicKeyToBytes(method.publicKeyJwk!);
-    final data = ['id=${method.id}', 't=$idx', 'k=${Base64Url.encode(pubKey)}']
-        .join(';');
+    final keyTypeIndex = _indexToKeyType[method.publicKeyJwk!.crv!];
+    final data = [
+      'id=${method.id.split('#').last}',
+      't=$keyTypeIndex',
+      'k=${Base64Url.encode(pubKey)}',
+    ].join(';');
 
     return Answer<TxtData>(
       name: RecordName('_k$idx._did'),
       type: RecordType.TXT,
       klass: RecordClass.IN,
       data: TxtData([data]),
-      ttl: 0,
+      ttl: 7200,
     );
   }
 
-  static DidVerificationMethod toDidVerificationMethod(Answer<TxtData> record) {
-    final txtData = record.data;
-    throw UnimplementedError();
+  static DidVerificationMethod toVerificationMethod(
+    String didId,
+    Answer<TxtData> record,
+  ) {
+    final map = {};
+
+    final fields = record.data.value.first.split(';');
+    for (final field in fields) {
+      final parts = field.split('=');
+      if (parts.length != 2) {
+        throw Exception('Invalid verification method format');
+      }
+
+      final [key, value] = parts;
+      map[key] = value;
+    }
+
+    final keyType = _indexToKeyType[map['t']];
+    if (keyType == null) {
+      throw Exception('Unsupported key type: ${map['t']}');
+    }
+
+    final pubKey = Crypto.bytesToPublicKey(keyType, Base64Url.decode(map['k']));
+    final vmIdFragment = map['id'] ?? pubKey.computeThumbprint();
+    return DidVerificationMethod(
+      id: '$didId#$vmIdFragment',
+      type: 'JsonWebKey',
+      controller: map['c'] ?? didId,
+      publicKeyJwk: pubKey,
+    );
   }
 }
---
+++ packages/web5/test/dids/did_dht/vm_record_test.dart
@@ -0,0 +1,30 @@
+import 'package:test/test.dart';
+import 'package:web5/src/dids/did_dht/dns_packet.dart';
+import 'package:web5/src/dids/did_dht/vm_record.dart';
+
+void main() {
+  group('VerificationMethodRecord', () {
+    group('toVerificationMethod', () {
+      test('should return a DidVerificationMethod', () {
+        final vector = Answer<TxtData>(
+          name: RecordName('_k0._did'),
+          type: RecordType.TXT,
+          klass: RecordClass.IN,
+          data: TxtData([
+            't=0;k=afdea69c63605863a68edea0ff7ff49dde0a96ce7e9249eb7780dd3d6f2ab5fc;a=Ed25519',
+          ]),
+          ttl: 7200,
+        );
+
+        final did =
+            'did:dht:i9xkp8ddcbcg8jwq54ox699wuzxyifsqx4jru45zodqu453ksz6y';
+        final vm = VerificationMethodRecord.toVerificationMethod(did, vector);
+
+        expect(vm.controller, equals(did));
+        expect(vm.id, contains('$did#'));
+        expect(vm.type, equals('JsonWebKey'));
+        expect(vm.publicKeyJwk!.crv, equals('Ed25519'));
+      });
+    });
+  });
+}
