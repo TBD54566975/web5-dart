af6aee3a2ac15880b8beacc8ba8e13078f3cfba0
Moe Jangda <moe@tbd.email>
2024-04-24 19:21:03 -0500

    make linter happy

--- packages/web5/test/dids/did_dht/converters/did_document_converter_test.dart
+++ packages/web5/test/dids/did_dht/converters/did_document_converter_test.dart
@@ -1,4 +1,3 @@
-import 'dart:math';
 
 import 'package:test/test.dart';
 import 'package:web5/src/dids/did_dht/dns_packet.dart';
@@ -145,7 +144,7 @@ void main() {
 
       final didDocument = DidDocumentConverter.convertDnsPacket(did, dnsPacket);
       expect(didDocument.id,
-          'did:dht:hpmp9uur565nkimpwdzom7ehbuabnsba658xwwynyk7awcd15bko');
+          'did:dht:hpmp9uur565nkimpwdzom7ehbuabnsba658xwwynyk7awcd15bko',);
       expect(didDocument.verificationMethod!.length, 3);
       expect(didDocument.authentication!.length, 2);
       expect(didDocument.assertionMethod!.length, 2);
