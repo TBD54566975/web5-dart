a8d2c57d77e804adfab6ea205481a351ccfd03e1
Ethan Lee <ethanl@tbd.email>
2024-04-25 15:24:43 -0700

    mock tests with http clients

--- packages/web5/test/dids/did_dht/did_dht_test.dart
+++ packages/web5/test/dids/did_dht/did_dht_test.dart
@@ -1,31 +1,20 @@
-import 'dart:io';
-
+import 'package:convert/convert.dart';
+import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
+
 import 'package:test/test.dart';
 import 'package:web5/web5.dart';
 
-class MockHttpClient extends Mock implements HttpClient {}
+import '../../helpers/mocks.dart';
 
-class MockHttpRequest extends Mock implements HttpClientRequest {}
-
-class MockHttpResponse extends Mock implements HttpClientResponse {}
-
-const validDidDhtDocument =
-    '''{id: did:dht:74hg1efatndi8enx3e4z6c4u8ieh1xfkyay4ntg4dg1w6risu35y, verificationMethod: [{id: 0, type: JsonWebKey2020, controller: did:dht:74hg1efatndi8enx3e4z6c4u8ieh1xfkyay4ntg4dg1w6risu35y, publicKeyJwk: {kty: OKP, alg: EdDSA, kid: a6tCQvXJQIZQZs_A126CcOT7PuP6R3yADH6DJLr1Zkg, crv: Ed25519, x: 7rhpILiIh1OgT8o1fzNTPVHJPKoGAaFE2hmlTxK2nnY}}], service: [{id: kyc-widget, type: kyc-widget, serviceEndpoint: http://localhost:5173}, {id: pfi, type: PFI, serviceEndpoint: http://localhost:8892/ingress/pfi}], assertionMethod: [0], authentication: [0], capabilityDelegation: [0], capabilityInvocation: [0]}''';
+const testVector =
+    '85ad53bb66db27eba9799d807a1dff1b43823263b72a0824aad94026980048ccbdfc3fdfe9355c243c32f5ed0f40ab3917b925783f6e49b6cd1a73333691e80c000000006625fa11000084000000000300000000035f6b30045f646964343377686674677062646a696878397a653974646e3537357a717a6d347177636365746e66317962696962757a61643772726d7979000010000100001c2000373669643d303b743d303b6b3d7a5468596d6145616138662d365078474c6664336464656e5559784552466b414e61686e66412d6b497341035f7330045f646964343377686674677062646a696878397a653974646e3537357a717a6d347177636365746e66317962696962757a61643772726d7979000010000100001c2000272669643d7066693b743d5046493b73653d68747470733a2f2f6c6f63616c686f73743a39303030045f646964343377686674677062646a696878397a653974646e3537357a717a6d347177636365746e66317962696962757a61643772726d7979000010000100001c20002e2d763d303b766d3d6b303b617574683d6b303b61736d3d6b303b64656c3d6b303b696e763d6b303b7376633d7330';
 
 void main() {
-  final MockHttpClient mockClient = MockHttpClient();
-  final MockHttpRequest request = MockHttpRequest();
-  final MockHttpResponse response = MockHttpResponse();
-
-  setUpAll(() {
-    registerFallbackValue(Uri());
-  });
+  late MockHttpClient mockHttpClient;
 
   setUp(() {
-    reset(mockClient);
-    reset(request);
-    reset(response);
+    mockHttpClient = MockHttpClient();
   });
 
   group('DidDht', () {
@@ -53,13 +42,38 @@ void main() {
     });
 
     test('should resolve with didDocument if legit', () async {
+      when(
+        () => mockHttpClient.get(
+          Uri.parse(
+            'https://diddht.tbddev.org/3whftgpbdjihx9ze9tdn575zqzm4qwccetnf1ybiibuzad7rrmyy',
+          ),
+        ),
+      ).thenAnswer(
+        (_) async =>
+            http.Response(String.fromCharCodes(hex.decode(testVector)), 200),
+      );
+
       final did = Did.parse(
         'did:dht:3whftgpbdjihx9ze9tdn575zqzm4qwccetnf1ybiibuzad7rrmyy',
       );
-      final resolutionResult = await DidDht.resolve(did);
+      final resolutionResult = await DidDht.resolve(
+        did,
+        client: mockHttpClient,
+      );
 
-      expect(resolutionResult.didResolutionMetadata.isEmpty(), isTrue);
       expect(resolutionResult.didDocument, isNotNull);
+      expect(
+        'did:dht:3whftgpbdjihx9ze9tdn575zqzm4qwccetnf1ybiibuzad7rrmyy',
+        resolutionResult.didDocument?.id,
+      );
+
+      verify(
+        () => mockHttpClient.get(
+          Uri.parse(
+            'https://diddht.tbddev.org/3whftgpbdjihx9ze9tdn575zqzm4qwccetnf1ybiibuzad7rrmyy',
+          ),
+        ),
+      ).called(1);
     });
   });
 }
