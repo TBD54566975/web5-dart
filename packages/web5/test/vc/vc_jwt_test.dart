cc09f7e4af42b03d77f80a670c3c3748406b6a3a
Diane Huxley <dhuxley@tbd.email>
2024-04-27 17:00:16 -0700

    vc package

---
+++ packages/web5/test/vc/vc_jwt_test.dart
@@ -0,0 +1,304 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+import 'package:web5/src/crypto.dart';
+import 'package:web5/src/vc/vc.dart';
+import 'package:web5/src/vc/vc_jwt.dart';
+import 'package:web5/web5.dart';
+
+void main() {
+  group('decode', () {
+    late BearerDid signerDid;
+    late VerifiableCredential vc;
+
+    setUp(() async {
+      signerDid = await DidJwk.create();
+      vc = VerifiableCredential.create(
+        data: {'foo': 'bar'},
+        issuer: 'did:ex:pfi',
+        subject: 'did:ex:alice',
+      );
+    });
+
+    test('returns a DecodedVcJwt with vc and jwt', () async {
+      final vcJwt = await vc.sign(signerDid);
+
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc, isNotNull);
+      expect(decodedVcJwt.jwt, isNotNull);
+    });
+
+    test('throws if vc-jwt is missing claims', () async {
+      // Sign without setting claims
+      final header = JwtHeader(typ: 'JWT');
+      final vcJwt =
+          await Jws.sign(did: signerDid, payload: Uint8List(0), header: header);
+
+      expect(() => DecodedVcJwt.decode(vcJwt), throwsException);
+    });
+
+    test('throws if vc-jwt is missing vc claims', () async {
+      signWithoutVcClaims(VerifiableCredential vc, BearerDid bearerDid) async {
+        final claims = JwtClaims(
+          iss: vc.issuer,
+          jti: vc.id,
+          sub: vc.subject,
+        );
+
+        final issuanceDateTime = DateTime.parse(vc.issuanceDate);
+        claims.nbf = issuanceDateTime.millisecondsSinceEpoch ~/ 1000;
+
+        if (vc.expirationDate != null) {
+          final expirationDateTime = DateTime.parse(vc.expirationDate!);
+          claims.exp = expirationDateTime.millisecondsSinceEpoch ~/ 1000;
+        }
+
+        // deliberately omit: claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final vcJwt = await signWithoutVcClaims(vc, signerDid);
+      expect(() => DecodedVcJwt.decode(vcJwt), throwsException);
+    });
+
+    test('sets vc issuer to jwt iss', () async {
+      signAndSetCustomIss(
+        VerifiableCredential vc,
+        BearerDid bearerDid,
+        String iss,
+      ) async {
+        final claims = JwtClaims(
+          iss: iss,
+          jti: vc.id,
+          sub: vc.subject,
+        );
+
+        final issuanceDateTime = DateTime.parse(vc.issuanceDate);
+        claims.nbf = issuanceDateTime.millisecondsSinceEpoch ~/ 1000;
+
+        if (vc.expirationDate != null) {
+          final expirationDateTime = DateTime.parse(vc.expirationDate!);
+          claims.exp = expirationDateTime.millisecondsSinceEpoch ~/ 1000;
+        }
+
+        claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final iss = 'did:ex:custom';
+      final vcJwt = await signAndSetCustomIss(vc, signerDid, iss);
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc.issuer, equals(iss));
+    });
+
+    test('sets vc id to jwt jti', () async {
+      signAndSetCustomJti(
+        VerifiableCredential vc,
+        BearerDid bearerDid,
+        String jti,
+      ) async {
+        final claims = JwtClaims(
+          iss: vc.issuer,
+          jti: jti,
+          sub: vc.subject,
+        );
+
+        final issuanceDateTime = DateTime.parse(vc.issuanceDate);
+        claims.nbf = issuanceDateTime.millisecondsSinceEpoch ~/ 1000;
+
+        if (vc.expirationDate != null) {
+          final expirationDateTime = DateTime.parse(vc.expirationDate!);
+          claims.exp = expirationDateTime.millisecondsSinceEpoch ~/ 1000;
+        }
+
+        claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final jti = 'custom-id';
+      final vcJwt = await signAndSetCustomJti(vc, signerDid, jti);
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc.id, equals(jti));
+    });
+
+    test('sets vc subject if jwt sub', () async {
+      signAndSetCustomSub(
+        VerifiableCredential vc,
+        BearerDid bearerDid,
+        String sub,
+      ) async {
+        final claims = JwtClaims(
+          iss: vc.issuer,
+          jti: vc.id,
+          sub: sub,
+        );
+
+        final issuanceDateTime = DateTime.parse(vc.issuanceDate);
+        claims.nbf = issuanceDateTime.millisecondsSinceEpoch ~/ 1000;
+
+        if (vc.expirationDate != null) {
+          final expirationDateTime = DateTime.parse(vc.expirationDate!);
+          claims.exp = expirationDateTime.millisecondsSinceEpoch ~/ 1000;
+        }
+
+        claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final sub = 'did:ex:custom';
+      final vcJwt = await signAndSetCustomSub(vc, signerDid, sub);
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc.subject, equals(sub));
+    });
+
+    test('sets vc expirationDate to jwt exp', () async {
+      signAndSetCustomExp(
+        VerifiableCredential vc,
+        BearerDid bearerDid,
+        int exp,
+      ) async {
+        final claims = JwtClaims(
+          iss: vc.issuer,
+          jti: vc.id,
+          sub: vc.subject,
+        );
+
+        final issuanceDateTime = DateTime.parse(vc.issuanceDate);
+        claims.nbf = issuanceDateTime.millisecondsSinceEpoch ~/ 1000;
+
+        claims.exp = exp;
+
+        claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final exp = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      final nowRounded =
+          DateTime.fromMillisecondsSinceEpoch(exp * 1000).toString();
+
+      final vcJwt = await signAndSetCustomExp(vc, signerDid, exp);
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc.expirationDate, equals(nowRounded));
+    });
+
+    test('sets vc issuanceDate to vc nbf', () async {
+      signAndSetCustomNbf(
+        VerifiableCredential vc,
+        BearerDid bearerDid,
+        int nbf,
+      ) async {
+        final claims = JwtClaims(
+          iss: vc.issuer,
+          jti: vc.id,
+          sub: vc.subject,
+        );
+
+        claims.nbf = nbf;
+
+        if (vc.expirationDate != null) {
+          final expirationDateTime = DateTime.parse(vc.expirationDate!);
+          claims.exp = expirationDateTime.millisecondsSinceEpoch ~/ 1000;
+        }
+        claims.misc = <String, dynamic>{'vc': vc.toJson()};
+
+        return await Jwt.sign(did: bearerDid, payload: claims);
+      }
+
+      final nbf = DateTime.now().millisecondsSinceEpoch ~/ 1000;
+      final nowRounded =
+          DateTime.fromMillisecondsSinceEpoch(nbf * 1000).toString();
+
+      final vcJwt = await signAndSetCustomNbf(vc, signerDid, nbf);
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+      expect(decodedVcJwt.vc.issuanceDate, equals(nowRounded));
+    });
+  });
+
+  group('verify', () {
+    late BearerDid signerDid;
+    late VerifiableCredential vc;
+    late DecodedVcJwt decodedVcJwt;
+
+    setUp(() async {
+      signerDid = await DidJwk.create();
+      vc = VerifiableCredential.create(
+        data: {'foo': 'bar'},
+        issuer: 'did:ex:pfi',
+        subject: 'did:ex:alice',
+      );
+      final vcJwt = await vc.sign(signerDid);
+      decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+    });
+
+    test('returns successfully for well-formed VcJwt', () async {
+      await expectLater(decodedVcJwt.verify(), completes);
+    });
+
+    test('throws if jwt.header.typ is not "JWT"', () async {
+      decodedVcJwt.jwt.header.typ = 'GARBAGE';
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if vc issuer is missing', () async {
+      decodedVcJwt.vc.issuer = '';
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if vc id is missing', () async {
+      decodedVcJwt.vc.id = '';
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if issuanceDate is in the future', () async {
+      final tomorrow = DateTime.now().add(Duration(hours: 24));
+      decodedVcJwt.vc.issuanceDate = tomorrow.toString();
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if expirationDate is in the past', () async {
+      final yesterday = DateTime.now().subtract(Duration(hours: 24));
+      decodedVcJwt.vc.expirationDate = yesterday.toString();
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if type is empty', () async {
+      decodedVcJwt.vc.type = [];
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if type does not contain base type', () async {
+      decodedVcJwt.vc.type = ['GARBAGE'];
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if context is empty', () async {
+      decodedVcJwt.vc.context = [];
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if context does not contain base context', () async {
+      decodedVcJwt.vc.context = ['GARBAGE'];
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+
+    test('throws if signature is wrong', () async {
+      var vcJwt = await vc.sign(signerDid);
+      final parts = vcJwt.split('.');
+      vcJwt = [
+        parts[0],
+        parts[1],
+        Base64Url.encode(List.filled(64, 0)), // malformed signature
+      ].join('.');
+      final decodedVcJwt = DecodedVcJwt.decode(vcJwt);
+
+      expect(() async => await decodedVcJwt.verify(), throwsException);
+    });
+  });
+}
---
+++ packages/web5/test/vc/vc_test.dart
@@ -0,0 +1,85 @@
+import 'package:test/test.dart';
+import 'package:web5/src/crypto.dart';
+import 'package:web5/src/vc/vc.dart';
+import 'package:web5/src/vc/vc_jwt.dart';
+import 'package:web5/web5.dart';
+
+void main() {
+  group('create', () {
+    test('uses default values for type, context, id, and issuanceDate',
+        () async {
+      final data = {'foo': 'bar'};
+      final issuer = 'did:ex:pfi';
+      final subject = 'did:ex:alice';
+      final vc = VerifiableCredential.create(
+        data: data,
+        issuer: issuer,
+        subject: subject,
+      );
+
+      expect(vc.data, equals(data));
+      expect(vc.subject, equals(subject));
+      expect(vc.issuer, equals(issuer));
+      expect(vc.type, equals(['VerifiableCredential']));
+      expect(vc.context, equals(['https://www.w3.org/2018/credentials/v1']));
+      expect(vc.issuanceDate, isNotNull);
+      expect(vc.id, startsWith('urn:vc:uuid:'));
+      expect(vc.expirationDate, isNull);
+      expect(vc.credentialSchema, equals([]));
+    });
+
+    test('accepts values passed in options', () async {
+      final data = {'foo': 'bar'};
+      final issuer = 'did:ex:pfi';
+      final subject = 'did:ex:alice';
+      final context = ['SomeContext'];
+      final type = ['SomeType'];
+      final id = 'urn:vc:uuid:1234';
+      final issuanceDate = DateTime.now().subtract(Duration(hours: 24));
+      final expirationDate = DateTime.now().subtract(Duration(hours: 12));
+      final credentialSchema = [
+        CredentialSchema(id: 'id', type: 'CredentialType')
+      ];
+
+      final vc = VerifiableCredential.create(
+        data: data,
+        issuer: issuer,
+        subject: subject,
+        context: context,
+        type: type,
+        id: id,
+        issuanceDate: issuanceDate,
+        expirationDate: expirationDate,
+        credentialSchema: credentialSchema,
+      );
+
+      expect(vc.data, equals(data));
+      expect(vc.subject, equals(subject));
+      expect(vc.issuer, equals(issuer));
+      expect(vc.type, equals(type));
+      expect(vc.context, equals(context));
+      expect(DateTime.parse(vc.issuanceDate), equals(issuanceDate));
+      expect(vc.id, equals(id));
+      expect(DateTime.parse(vc.expirationDate!), equals(expirationDate));
+      expect(vc.credentialSchema, equals(credentialSchema));
+    });
+  });
+
+  group('sign', () {
+    test('returns a JWT signed by the BearerDid', () async {
+      final did = await DidJwk.create();
+
+      final data = {'foo': 'bar'};
+      final issuer = did.uri;
+      final subject = 'did:ex:alice';
+      final vc = VerifiableCredential.create(
+        data: data,
+        issuer: issuer,
+        subject: subject,
+      );
+
+      final vcJwt = await vc.sign(did);
+      await expectLater(Jwt.decode(vcJwt).verify(), completes);
+    });
+  });
+}
