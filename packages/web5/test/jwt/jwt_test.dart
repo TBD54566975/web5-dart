9b2b5fcca19bf5d0a43f7f2e925176f335e241cf
Ethan Lee <ethanl@tbd.email>
2024-05-07 14:33:16 -0700

    test fails when signing with `DidDht`

--- packages/web5/test/jwt/jwt_test.dart
+++ packages/web5/test/jwt/jwt_test.dart
@@ -14,13 +14,24 @@ void main() {
       expect(parsedJwt.claims.iss, equals(did.uri));
     });
 
-    test('should verify signed JWT', () async {
+    test('should verify JWT signed by did:jwk', () async {
       final did = await DidJwk.create();
 
       final signedJwt =
           await Jwt.sign(did: did, payload: JwtClaims(iss: did.uri));
 
       await Jwt.verify(signedJwt);
+    });
+
+    test('should verify signed JWT signed by did:dht', () async {
+      final did = await DidDht.create(publish: true);
+
+      final signedJwt =
+          await Jwt.sign(did: did, payload: JwtClaims(iss: did.uri));
+
+      final parsedJwt = Jwt.decode(signedJwt);
+      expect(parsedJwt.header.kid, contains('${did.uri}#'));
+      expect(parsedJwt.claims.iss, equals(did.uri));
     });
   });
 }
